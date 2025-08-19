48f09d2f3ca5b4356d1367dec798035ef4e7884d
semteto <brennodmoraes@gmail.com>
2025-08-19 18:22:48 -0300

    FIX: Downgrade do barcode-scanning para versão compatível

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -8,26 +8,20 @@ const config: CapacitorConfig = {
     url: "https://www.appshapefit.com",
     cleartext: true
   },
-  // ======================================================
-  // INÍCIO DA SOLUÇÃO - DEFININDO O CANAL DE NOTIFICAÇÃO
-  // ======================================================
   plugins: {
     PushNotifications: {
       presentationOptions: ["badge", "sound", "alert"],
-      // A parte abaixo cria o canal "Padrão" no Android
-      // sem isso, o Android 8+ pode ignorar a notificação
       channels: [
         {
-          id: "default_channel", // ID interno do canal
-          name: "Padrão",        // Nome que o usuário vê nas configurações
-          importance: 5,         // Importância máxima (5) para garantir que apareça
-          visibility: 1,         // Torna a notificação pública
-          sound: "default"       // Som padrão
+          id: "default_channel",
+          name: "Padrão",
+          importance: 5,
+          visibility: 1,
+          sound: "default"
         }
       ]
     }
   }
-  // ======================================================
 };
 
 export default config;
